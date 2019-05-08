e5e255c87bb01eaba208424020b4abe0ebba0d91
Aluno <gabrielmelocomp@gmail.com>
2019-05-08 07:30:28 -0300

    Added class #4 Challenge (ComputerPizza)

---
+++ scripts/class-4/computer_pizza.py
@@ -0,0 +1,13 @@
+# O valor total da pizza é calculado por: 
+# 	total = valor da pizza + taxa de entrega
+#	
+# Onde o valor da entrega é dado por:
+#	taxa de entrega = valor da pizza * 15/100 (15%)
+
+preco_pizza = input("Insira o valor da pizza")
+
+taxa_entrega = ???
+
+valor_total = ???
+
+print("O valor total a ser pago é de:", valor_total)
