8b7d8f23df1f80ea76ffaf6869adde0efb627c20
Aluno <gabrielmelocomp@gmail.com>
2019-05-08 07:25:08 -0300

    Added ackowledgement.py script to class 4

---
+++ scripts/class-4/acknowledgement.py
@@ -0,0 +1,10 @@
+nome_usuario = input("Digite seu nome:\n")
+idade_usuario = input("Digite sua idade:\n")
+
+nome_eu = "Gabriel"
+idade_eu = 23
+
+print("Olá,", nome_usuario + ".")
+print("Você possui", idade_usuario, "anos de idade.")
+print("Eu me chamo", nome_eu, "e possuo", idade_eu, "anos de idade.")
+print("Obrigado por me ler até o final,", nome_usuario + ".") 
