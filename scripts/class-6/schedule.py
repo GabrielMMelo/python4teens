287f79ada2f8ded669f6e26210e1407202e8ff80
Gabriel M. de Melo <gabrielmelocomp@gmail.com>
2019-06-11 09:06:06 -0300

    Added class #6

---
+++ scripts/class-6/schedule.py
@@ -0,0 +1,18 @@
+dia_semana = input("Olá Sr.(a)\n Insira o dia da semana que deseja verificar seus compromissos. Digite apenas as três primeiras letras (e.g., 'seg' para segunda-feira, ou 'sab' para sábado):\n")
+if dia_semana == 'seg':
+    print("Segunda, cuidado para não ficar corcunda")
+elif dia_semana == 'ter':
+    print("Na terça é esperado que você exerça")
+elif dia_semana == 'qua':
+    print("Na quarta, tem jogo do Brasil e gol da Marta")
+elif dia_semana == 'qui':
+    print("Quinta, como pinóquio nos ensinou: não minta!")
+elif dia_semana == 'sex':
+    print("É sexta, não precisa rimar.")
+elif dia_semana == 'sab':
+    print("No sábado, vem tranquilo, não vem afobado")
+elif dia_semana == 'dom':
+    print("Se não fizer o dever de segunda, vai levar xingo")
+else:
+    print("Eita, não entendi")
+print("Bye bye. Até mais!")
---
+++ scripts/class-6/ultra-mega-secret-party.py
@@ -0,0 +1,5 @@
+idade = input("Bom dia, Sr.(a)\nEssa é a entrada para a festa ultra mega secreta. Para entrar, digite sua idade:\n")
+if int(idade) >= 18:
+    print("Parabéns, você pode entrar na festa, porém ela não é de verdade huehuehue")
+else:
+    print("Infelizmente você não possui os requisitos mínimos para adentrar nessa festa ultra mega secreta.")
